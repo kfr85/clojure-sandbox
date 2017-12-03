038ca0ea6470e949a98beb0fb193428ab87aef4d
rusagedougawa <rusa.gedougawa@gmail.com>
2017-12-04 01:02:34 +0900

    mv

--- test/clojure_sandbox/core_test.clj
+++ test/clojure_sandbox/core_test.clj
@@ -1,6 +1,6 @@
-(ns clojure-app.core-test
+(ns clojure-sandbox.core-test
   (:require [clojure.test :refer :all]
-            [clojure-app.core :refer :all]))
+            [clojure-sandbox.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
