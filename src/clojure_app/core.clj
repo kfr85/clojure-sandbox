6d1c98a04f8c50dece14dd11fea388ee411192c4
rusagedougawa <rusa.gedougawa@gmail.com>
2017-12-04 00:50:38 +0900

    init commit

---
+++ src/clojure_app/core.clj
@@ -0,0 +1,39 @@
+(ns clojure-app.core 
+  (:require [clojure.java.io :as io]
+            [clojure.core.async :as async])
+  (:import (java.net ServerSocket)))
+
+; ソケット通信
+(def port 8080)
+
+; fiszbuzz
+(defn fizzbuzz
+  [num]
+  (cond 
+    (= (mod num 15) 0) "FizzBuzz"
+    (= (mod num 5) 0) "Fizz"
+    (= (mod num 3) 0) "Buzz"
+    :else (str num)))
+
+(defn -main
+  "http server写経するよ"
+  [& args]
+  ;(println 
+  ;  (map fizzbuzz 
+  ;    (range 1 (+ 1 (read-string (first args))))))
+  (let [server-socket (new ServerSocket port)]
+    (prn (str "hello http server" port "!"))
+    (while true
+      (let [socket (.accept server-socket)]
+        (async/thread
+          (with-open [s socket
+                      in (io/input-stream (.getInputStream s))
+                      out (io/output-stream (.getOutputStream s))]
+            (prn "in:" in "out:" out)
+          )
+        )
+      )
+    )
+  )
+)
+
